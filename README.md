5300be86948524a05bdcea04c84824eca3ab788a
radiantly <buildingsomethingfun@gmail.com>
2021-11-28 23:22:56 +0530

    Delegate serving static files to nginx
    Also add instructions to setup the server in the readme (thanks to
    @chebro for the systemd unit file and nginx config that this based
    on).

--- README.md
+++ README.md
@@ -2,6 +2,94 @@
 
 Or can you? Visit https://youcantdownloadthisimage.online/ to give it a go!
 
+## Running your own server
+
+If you'd just like to test the code that keeps the connection open, run the following
+
+```sh
+# Install dependencies and start serving lisa.jpg on http://localhost:3000/
+cargo run
+```
+
+<details>
+<summary>Full setup</summary>
+The following assumes that you have nginx installed on a linux system with systemd.
+
+```sh
+# Clone this repository
+git clone https://github.com/radiantly/you-cant-download-this-image
+cd you-cant-download-this-image
+
+# Release build
+cargo build --release
+```
+
+Create a systemd unit file to keep it running:
+
+These are the contents of `/etc/systemd/system/lisa.service` _(replace paths as needed)_
+
+```
+[Unit]
+Description=You can't download this image server
+After=network.target
+
+[Service]
+Type=simple
+WorkingDirectory=/root/you-cant-download-this-image
+ExecStart=/root/you-cant-download-this-image/target/release/you-cant-download-this-image
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+```
+
+```sh
+systemctl daemon-reload   # Reload service files on diskfi
+systemctl start lisa      # Start
+systemctl enable lisa     # Atuostart on boot
+```
+
+For the ssl cert, follow the instructions on Certbot's website.
+
+Finally, something like this can be added to the nginx config:
+
+```
+server {
+    if ($host = www.youcantdownloadthisimage.online) {
+        return 301 https://$host$request_uri;
+    } # managed by Certbot
+
+
+    if ($host = youcantdownloadthisimage.online) {
+        return 301 https://$host$request_uri;
+    } # managed by Certbot
+
+    listen 80;
+}
+
+server {
+    listen 443 ssl;
+    root /root/you-cant-download-this-image/public;
+    server_name youcantdownloadthisimage.online www.youcantdownloadthisimage.online;
+    ssl_certificate /etc/letsencrypt/live/youcantdownloadthisimage.online/fullchain.pem; # managed by Certbot
+    ssl_certificate_key /etc/letsencrypt/live/youcantdownloadthisimage.online/privkey.pem; # managed by Certbot
+
+    include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
+
+    location / {
+        add_header X-Frame-Options "SAMEORIGIN";
+        add_header Access-Control-Allow-Origin https://youcantdownloadthisimage.online;
+    }
+
+    location /lisa.jpg {
+        proxy_pass              http://localhost:3000;
+        proxy_redirect          http://localhost:3000 https://youcantdownloadthisimage.online;
+    }
+}
+```
+
+</details>
+
 ## License
 
 MIT
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 use hyper::body::{Bytes, Sender};
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use hyper::{Body, Request, Response, Server};
 use lazy_static::lazy_static;
 use std::cmp::max;
-use std::collections::{HashMap, VecDeque};
+use std::collections::VecDeque;
 use std::convert::Infallible;
-use std::ffi::OsStr;
 use std::fs;
-use std::fs::read_dir;
 use std::net::SocketAddr;
 use std::sync::{Arc, Mutex};
 use std::thread;
@@ -16,21 +14,6 @@ use std::time::{Duration, SystemTime, UNIX_EPOCH};
 lazy_static! {
     static ref SENDERS: Arc<Mutex<VecDeque<(Sender, u128)>>> =
         Arc::new(Mutex::new(VecDeque::new()));
-    static ref STATIC_FILES: HashMap<String, Vec<u8>> = init_static_files();
-}
-
-fn init_static_files() -> HashMap<String, Vec<u8>> {
-    let mut static_files = HashMap::new();
-    for path in read_dir("./public/").unwrap() {
-        let path = path.unwrap();
-        if path.file_type().unwrap().is_file() {
-            static_files.insert(
-                format!("/{}", OsStr::to_string_lossy(path.file_name().as_os_str())),
-                fs::read(path.path()).unwrap(),
-            );
-        }
-    }
-    static_files
 }
 
 fn time_now() -> u128 {
@@ -40,43 +23,25 @@ fn time_now() -> u128 {
         .as_millis()
 }
 
-async fn handle_request(req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    if req.method() != &Method::GET {
-        let mut response = Response::new(Body::empty());
-        *response.status_mut() = StatusCode::METHOD_NOT_ALLOWED;
-        return Ok(response);
+async fn handle_request(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    lazy_static! {
+        static ref IMG_DATA: Vec<u8> = fs::read("./public/lisa.jpg").unwrap();
     }
-
-    let path = if req.uri().path() == "/" {
-        "/index.html"
-    } else {
-        req.uri().path()
-    };
-
-    if path == "/lisa.jpg" {
-        let (mut chan, body) = Body::channel();
-        let img_data = STATIC_FILES.get("/lisa.jpg").unwrap();
-        if chan.send_data(Bytes::from(&img_data[..])).await.is_ok() {
-            SENDERS.lock().unwrap().push_back((chan, time_now()));
-        }
-        return Ok(Response::builder()
-            .header("X-Accel-Buffering", "no")
-            .body(body)
-            .unwrap());
+    let (mut chan, body) = Body::channel();
+    if chan.send_data(Bytes::from(&IMG_DATA[..])).await.is_ok() {
+        SENDERS.lock().unwrap().push_back((chan, time_now()));
     }
-
-    if let Some(contents) = STATIC_FILES.get(path) {
-        return Ok(Response::new(Body::from(&contents[..])));
-    }
-
-    let mut response = Response::new(Body::empty());
-    *response.status_mut() = StatusCode::NOT_FOUND;
-    Ok(response)
+    return Ok(Response::builder()
+        .header("X-Accel-Buffering", "no")
+        .header("Cache-Control", "no-store")
+        .header("Content-Type", "image/jpeg")
+        .body(body)
+        .unwrap());
 }
 
 #[tokio::main]
 async fn main() {
-    // We'll bind to 127.0.0.1:3000
+    // We'll bind to port 3000
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
 
     thread::spawn(|| {
@@ -99,8 +64,6 @@ async fn main() {
             thread::sleep(Duration::from_millis(calc_wait_time() as u64));
         }
     });
-
-    // println!("{:?}", *STATIC_FILES);
 
     let make_svc =
         make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle_request)) });
